c4c12a4f568a813731d8d3bfde5f95ddd7b81681
piyushagarwal-55 <piyushaga2005@gmail.com>
2025-07-15 20:12:36 +0530

    production deployed

--- app/components/ModernAuthScreen.tsx
+++ app/components/ModernAuthScreen.tsx
@@ -496,9 +496,9 @@ const ModernAuthScreen: React.FC<ModernAuthScreenProps> = ({
       <View style={styles.madeWithContainer}>
         <Text style={styles.madeWithCreative}>
           Crafted with ❤️ & ☕ by{" "}
-          <Text style={styles.creatorNames}>Amrendra</Text>
+          <Text style={styles.creatorNames}>Piyush</Text>
           {" & "}
-          <Text style={styles.creatorNames}>Piyush!</Text>
+          <Text style={styles.creatorNames}>Amrendra</Text>
         </Text>
       </View>
     </SafeAreaView>
